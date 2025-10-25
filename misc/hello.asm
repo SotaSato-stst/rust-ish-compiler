24e0d99f506c4d405b906657d73a056d3b250452
SotaSato-stst <sota.sato@plaid.co.jp>
2025-10-26 00:39:51 +0900

    fix: sample

--- misc/hello.asm
+++ misc/hello.asm
@@ -1,23 +1,34 @@
-; macOS x86_64用 "Hello, World!" プログラム (NASM形式)
+global _main
+default rel
 
-global _main      ; プログラムのエントリポイントを定義
-default rel       ; RIP相対アドレッシングをデフォルトにする (モダンなmacOSで推奨)
+; 出力したいデータを格納する領域
+section .data
+    ; 1バイトの数字と改行コードを格納するバッファ
+    ; 初期値はダミーで、実行時に書き換えられます。
+    output_buffer db " ", 0x0a
 
-section .data     ; データセクション
-    msg db "Hello, World!", 0x0A ; 出力する文字列と改行コード(0x0A)
-    msg_len equ $ - msg         ; 文字列の長さ
+section .text
+_main:
+    mov rdi, 1
+    mov rsi, 2
+    call sum
+    add al, '0'
 
-section .text     ; コードセクション
-_main:            ; main関数に相当するエントリポイント
+    lea rdi, output_buffer
+    mov [rdi], al
 
-; 1. writeシステムコール (システムコール番号: 0x2000004)
-    mov rax, 0x2000004  ; raxにwriteのシステムコール番号を設定
-    mov rdi, 1          ; rdiに第一引数: ファイルディスクリプタ (1: 標準出力/stdout)
-    lea rsi, [msg]      ; rsiに第二引数: 出力する文字列のアドレス（RIP相対）
-    mov rdx, msg_len    ; rdxに第三引数: 出力するバイト数
-    syscall             ; システムコールを実行
+    mov rax, 0x2000004
+    mov rdi, 1
+    lea rsi, output_buffer
+    mov rdx, 2
+    syscall
 
-; 2. exitシステムコール (システムコール番号: 0x2000001)
-    mov rax, 0x2000001  ; raxにexitのシステムコール番号を設定
-    xor rdi, rdi        ; rdiに第一引数: 終了コード (0)
-    syscall             ; システムコールを実行
+    ; 5. exit システムコール
+    mov rax, 0x2000001
+    xor rdi, rdi
+    syscall
+
+sum:
+    mov rax, rdi
+    add rax, rsi
+    ret
